6090b1a553c6f6eaad9465b7b7e639985ad5909a
YanGeng <18516054923@163.com>
2015-05-23 16:28:18 +0800

    Add a new file

--- Word_Search/Solution.cpp
+++ Word_Search/Solution.cpp
@@ -7,41 +7,48 @@ using namespace std;
 
 class Solution {
 	public:
-	    bool exist(vector< vector<char> >& board, string word) {
-			int yl = board.size();
-			int xl = board[0].size();
-
-			map<char, int> myMap;
-			for (int i = 0; i < xl; ++i) {
-				for (int j = 0; j < yl; ++j) {
-					if (myMap.find(board[i][j]) == myMap.end()) {
-						myMap[board[i][j]]++;
-					} else {
-						myMap.insert(make_pair(board[i][j], 1));
-					}
-				}
+	bool exist(vector<vector<char> > &board, string word) {
+		m=board.size();
+		n=board[0].size();
+		for(int x=0;x<m;x++)
+			for(int y=0;y<n;y++)
+			{
+				if(isFound(board,word.c_str(),x,y))
+					return true;
 			}
 
-			for (int i = 0; i < word.size(); ++i) {
-				char tmp = word[i];
-				if (myMap.find(tmp) == myMap.end()) {
-					return false;
-				} else {
-					myMap[tmp]--;
-					if (myMap[tmp] == 0) {
-						myMap.erase(tmp);
-					}
-				}
-			}
+		return false;
+	}
 
+	private:
+	int m;
+	int n;
+	bool isFound(vector<vector<char> > &board, const char* w, int x, int y)
+	{
+		if(x<0||y<0||x>=m||y>=n||board[x][y]=='\0'||*w!=board[x][y])
+			return false;
+
+		if(*(w+1)=='\0')
 			return true;
-	    }
+
+		char t=board[x][y];
+		board[x][y]='\0';
+		if(isFound(board,w+1,x-1,y)||isFound(board,w+1,x+1,y)||isFound(board,w+1,x,y-1)||isFound(board,w+1,x,y+1))
+			return true; 
+
+		board[x][y]=t;
+		return false;
+	}
 };
 
 int main () {
 	vector< vector<char> > tmp;
-	tmp[0][0] = 'a';
-	tmp[0][1] = 'a';
+	//tmp[0][0] = 'a';
+	//tmp[0][1] = 'a';
+	vector<char> v;
+	v.push_back('a');
+	v.push_back('a');
+	tmp.push_back(v);
 
 	string word = "a";
 
